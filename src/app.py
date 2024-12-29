84f349da7881d1733a1dbd4878d0f2471c92f03d
k22o <kana_t3@yahoo.co.jp>
2024-12-29 21:22:03 +0900

    :sparkles: map

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ from src.domain.LatLon import LatLon
 from src.domain.Geohash import Geohash
 from src.domain.Meshcode import Meshcode
 from src.domain.CityMaster import CityMaster
+import src.map_util as map_util
+
 
 def showGeohash():
     st.title('Geohash')
@@ -19,17 +21,22 @@ def showGeohash():
     geohash4 = Geohash.encode_from_lat_lon(latLon, 4)
     geohash5 = Geohash.encode_from_lat_lon(latLon, 5)
     geohash6 = Geohash.encode_from_lat_lon(latLon, 6)
+    range4 = geohash4.decode_to_lat_lon()
+    range5 = geohash5.decode_to_lat_lon()
+    range6 = geohash6.decode_to_lat_lon()
     st.info(f'4次:   {geohash4.geohash}')
     st.info(f'5次:   {geohash5.geohash}')
     st.info(f'6次:   {geohash6.geohash}')
+    map_util.plot_point(latLon, [range4, range5, range6])
 
+    
     st.header('Geohash -> LatLon Range')
-    geohash_str = st.text_input(label='geohsh', value='xp5e')
+    geohash_str = st.text_input(label='geohash', value='xp5e')
     geohash_obj = Geohash(geohash_str)
     range = geohash_obj.decode_to_lat_lon()
     st.info(f'緯度 : {range["lat_min"]} ~ {range["lat_max"]}')
     st.info(f'経度 : {range["lon_min"]} ~ {range["lon_max"]}')
-
+    map_util.plot_area(range)
 
 
 def showMeshcode():
@@ -42,9 +49,14 @@ def showMeshcode():
     meshcode4 = Meshcode.encode_from_lat_lon(latLon, 4)
     meshcode6 = Meshcode.encode_from_lat_lon(latLon, 6)
     meshcode8 = Meshcode.encode_from_lat_lon(latLon, 8)
+    range4 = meshcode4.decode_to_lat_lon()
+    range6 = meshcode6.decode_to_lat_lon()
+    range8 = meshcode8.decode_to_lat_lon()
     st.info(f'1次:   {meshcode4.meshcode}')
     st.info(f'2次:   {meshcode6.meshcode}')
     st.info(f'3次:   {meshcode8.meshcode}')
+    map_util.plot_point(latLon, [range4,range6, range8])
+
 
     st.header('Meshcode -> LatLon Range')
     meshcode_str = st.text_input(label='meshcode', value='6040')
@@ -52,15 +64,10 @@ def showMeshcode():
     range = meshcode_obj.decode_to_lat_lon()
     st.info(f'緯度 最小: {range["lat_min"]} 最大: {range["lat_max"]}')
     st.info(f'経度 最小: {range["lon_min"]} 最大: {range["lon_max"]}')
+    map_util.plot_area(range)
 
-def showMap():
-    st.title('Map')
 
-    # 地図を描画する
-    # 緯度経度を取る
-    # lat = 40
-    # lon = 140
-    # latLon = LatLon(lat, lon)
+
 
 def showCity():
     st.title('City')
@@ -84,7 +91,6 @@ if __name__ == "__main__":
     contents = [
         "geohash",
         "meshcode",
-        "map",
         "city"
     ]
 
@@ -94,7 +100,5 @@ if __name__ == "__main__":
         showGeohash()
     elif page == "meshcode":
         showMeshcode()
-    elif page == "map":
-        showMap()
     elif page == "city":
         showCity()
